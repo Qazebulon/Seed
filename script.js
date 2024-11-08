67ddeea256ba79629d0e7acd85f0b33d79e11b46
Qazebulon <jonathan.stoner@outlook.com>
2024-11-08 17:06:48 -0600

    Initial testing -- full screen mode is not working

---
+++ script.js
@@ -0,0 +1,4 @@
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => { navigator.serviceWorker.register('/service-worker.js').then(reg => console.log('Service Worker registered:', reg)).catch(err => console.error('Service Worker registration failed:', err));
+	});
+}
